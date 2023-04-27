66944ddb61d153aac3fa73e45fc07cef9f7df5b8
intaniamentari <intan.appkey@gmail.com>
2023-04-27 14:11:25 +0800

    Im study here

--- streaming-client-api.js
+++ streaming-client-api.js
@@ -5,37 +5,48 @@ if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and 
 
 const RTCPeerConnection = (window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection).bind(window);
 
-let peerConnection;
+let peerConnection; //this have function for change this value
 let streamId;
 let sessionId;
 let sessionClientAnswer;
 
 
 const talkVideo = document.getElementById('talk-video');
-talkVideo.setAttribute('playsinline', '');
-const peerStatusLabel = document.getElementById('peer-status-label');
-const iceStatusLabel = document.getElementById('ice-status-label');
-const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label');
-const signalingStatusLabel = document.getElementById('signaling-status-label');
+talkVideo.setAttribute('playsinline', ''); // pada tag ditambahkan atribut ini
+const peerStatusLabel = document.getElementById('peer-status-label'); //Peer connection status
+const iceStatusLabel = document.getElementById('ice-status-label'); // ICE status
+const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label'); //ICE gathering status
+const signalingStatusLabel = document.getElementById('signaling-status-label');// Signaling status
 
+
+/**
+ * ````````````````````````````````````````
+ * Function "Button Connect"
+ * every connecton configuration must connected
+ * to connected with :
+ * 1. ICE gathering status
+ * 2. ICE status
+ * 3. Peer connection status
+ * 4. Signaling status
+ */
 const connectButton = document.getElementById('connect-button');
 connectButton.onclick = async () => {
-  if (peerConnection && peerConnection.connectionState === 'connected') {
+  if (peerConnection && peerConnection.connectionState === 'connected') { // status terkini dari koneksi antara perangkat lokal dan perangkat jarak jauh.
     return;
   }
 
-  stopAllStreams();
+  stopAllStreams(); //this function call on bottom
   closePC();
 
-  const sessionResponse = await fetch(`${DID_API.url}/talks/streams`, {
+  const sessionResponse = await fetch(`${DID_API.url}/talks/streams`, { // fetch to make HTTP request to endpoint in method POST
     method: 'POST',
     headers: {'Authorization': `Basic ${DID_API.key}`, 'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      source_url: "https://d-id-public-bucket.s3.amazonaws.com/or-roman.jpg"
+    body: JSON.stringify({//change js object to JSON
+      source_url: "https://create-images-results.d-id.com/DefaultPresenters/Emma_f/image.jpeg"
     }),
   });
 
-  
+  //response ini digunakan di berbagai function lainnya
   const { id: newStreamId, offer, ice_servers: iceServers, session_id: newSessionId } = await sessionResponse.json()
   streamId = newStreamId;
   sessionId = newSessionId;
@@ -56,6 +67,10 @@ connectButton.onclick = async () => {
       body: JSON.stringify({answer: sessionClientAnswer, session_id: sessionId})
     });
 };
+/**
+ * `````````````````````````````````````````````````````````````````````````````````````````
+ * END of Conection Button
+ */
 
 const talkButton = document.getElementById('talk-button');
 talkButton.onclick = async () => {
@@ -67,12 +82,21 @@ talkButton.onclick = async () => {
         headers: { Authorization: `Basic ${DID_API.key}`, 'Content-Type': 'application/json' },
         body: JSON.stringify({
           'script': {
-            'type': 'audio',
-            'audio_url': 'https://d-id-public-bucket.s3.us-west-2.amazonaws.com/webrtc.mp3',
+            'type': 'text',
+            'provider': {
+				type: 'microsoft',
+				voice_id: 'Jenny'
+			},
+			input: 'A butterfly is a type of insect that belongs to the order Lepidoptera, which also includes moths. Butterflies are known for their vibrant colors and unique patterns on their wings, which are made up of tiny scales. These scales reflect light and give the butterfly its signature appearance. Butterflies undergo a process of metamorphosis, which includes four distinct stages: egg, larva, pupa, and adult. During the larva stage, the butterfly takes the form of a caterpillar and feeds on plants. Once it enters the pupa stage, it forms a chrysalis or cocoon where it undergoes a transformation into an adult butterfly. Butterflies are important pollinators, helping to fertilize plants as they feed on nectar. They also play a significant role in the ecosystem as a food source for other animals. Due to habitat destruction and other factors, many species of butterflies are currently at risk of extinction, making their conservation an important concern for biodiversity.',
+			ssml: 'false'
+            // 'type': 'audio',
+            // 'audio_url': 'https://d-id-public-bucket.s3.us-west-2.amazonaws.com/webrtc.mp3',
           },
           'driver_url': 'bank://lively/',
           'config': {
-            'stitch': true,
+            // 'stitch': true,
+			fluent: 'false',
+			pad_audio: '0.0'
           },
           'session_id': sessionId
         })
@@ -131,6 +155,11 @@ function onTrack(event) {
   setVideoElement(remoteStream);
 }
 
+/**
+ * Anonymous function to set the value of let peerConnection
+ * 
+ * 
+ */
 async function createPeerConnection(offer, iceServers) {
   if (!peerConnection) {
     peerConnection = new RTCPeerConnection({iceServers});
@@ -164,14 +193,23 @@ function setVideoElement(stream) {
   }
 }
 
+/**
+ * stopAllStreams
+ * 
+ * to stop video an set in null (i think it reset function)
+ */
 function stopAllStreams() {
-  if (talkVideo.srcObject) {
+  if (talkVideo.srcObject) { //to get or set media like video or audio
     console.log('stopping video streams');
     talkVideo.srcObject.getTracks().forEach(track => track.stop());
     talkVideo.srcObject = null;
   }
 }
 
+/**
+ * 
+ * peer connection ini ada di let untuk get atau set video
+ */
 function closePC(pc = peerConnection) {
   if (!pc) return;
   console.log('stopping peer connection');
